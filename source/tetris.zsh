419ac3af5e299d583e68322284deceaff13e7472
JV <jaiveer.chadda@gmail.com>
2026-05-04 12:39:46 +0100

    Fixed a bug with the `EXIT` signal trap, and moved a few things into their own functions

--- source/tetris.zsh
+++ source/tetris.zsh
@@ -1,5 +1,7 @@
 #!/usr/bin/env zsh
 
+# ——————————————————————————————————————————————————————————————————————————— #
+
 tetris::buffer_setup() {
   local -r alt_buffer_on=$'\e[?1049h'
   local -r hide_cursor=$'⁠\e[?25l'
@@ -16,6 +18,38 @@ tetris::buffer_cleanup() {
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
+tetris::check_tty_size() {
+  local -ri 10  width=$display_width
+  local -ri 10 height=$display_height
+
+  if (( COLUMNS < width || LINES < height )) {
+    {
+      echo 'tetris: Screen too small'
+      echo "\tScreen has to be at least : ${(l:3:)width} cols x $height rows"
+      echo "\tScreen is currently       : ${(l:3:)COLUMNS} cols x $LINES rows"
+    } >&2
+    return 1
+  }
+}
+
+# ——————————————————————————————————————————————————————————————————————————— #
+
+tetris::display_setup() {
+  local -ri 10 padding_x=$(( ( COLUMNS - display_width  ) / 2 ))
+  local -ri 10 padding_y=$(( ( LINES   - display_height ) / 2 ))
+
+  local -r NL=$'\n'
+  local -r x_pad_str="${(r:$padding_x:)}"
+  local -r y_pad_str="${(pr:$padding_y::$NL:)}"
+
+  # vertical offset of the grid
+  echo "$y_pad_str"
+  # add the padding before the first line, and after every newline
+  echo "$x_pad_str${display//$NL/$NL$x_pad_str}"
+}
+
+# ——————————————————————————————————————————————————————————————————————————— #
+
 tetris() {
 
   # — Admin & Setup ————————————————————————————————————————————————————————— #
@@ -25,27 +59,43 @@ tetris() {
   setopt warn_create_global  # warn if one of the vars below will be global
   setopt warn_nested_var     # warn if a child process overwrites a local var
 
-  # exit the buffer and re-show the cursor if the program ever stops
-  trap 'tetris::buffer_cleanup' EXIT SIG{INT,QUIT,TERM,HUP,TSTP}
-  # restart buffer and re-hide cursor if we exited with ^Z, then come back
+  # EXIT doesn't need to be sent `kill`, so just clean up the buffer
+  trap 'tetris::buffer_cleanup' EXIT
+  # restart the buffer etc. if we exited with ^Z, then return with `fg`
   trap 'tetris::buffer_setup' SIGCONT
 
-  # set up the buffer and hide the cursor
-  tetris::buffer_setup
+  local sig
+  # I know the 'SIG' in SIGINT, SIGTERM, etc. is superfluous,
+  #  but I've left it in for clarity
+  for sig in SIG{INT,TERM,QUIT,HUP,TSTP}; {
+    # clean up buffer, unset the trap, and send the signal to the shell (`$$`)
+    trap "tetris::buffer_cleanup; trap - $sig; kill -$sig \$\$" $sig
+  }
+
+  # — Import, Verify, & Display Screen ————————————————————————————————————— #
+
+  local -r display="$( cat ../resources/out/display.txt )"
+
+  local -ri 10 display_width=${#display/%$'\n'*}
+  local -ri 10 display_height=$(( "${#display//[^$'\n']}" + 1 ))
+
+  # make sure the screen's large enough to fit the display
+  tetris::check_tty_size || return 1
+
+  # ————————————————————————————————————————————— #
+
+  tetris::buffer_setup   # set up the buffer and hide the cursor
+  tetris::display_setup  # print the base display, without anything filled in
 
   # ————————————————————————————————————————————————————————————————————————— #
 
-  sleep 1
-  echo 'this is some text'
-  sleep 1
-  echo 'in fact, there'\''s more text'
-  sleep 1
-
+  while { :; }
 }
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
-if [[ $ZSH_EVAL_CONTEXT == 'toplevel' ]] tetris
+# if __name__ == "__main__"
+if [[ "$ZSH_EVAL_CONTEXT" == 'toplevel' ]] tetris "$@"
 
 # ——————————————————————————————————————————————————————————————————————————— #
 
